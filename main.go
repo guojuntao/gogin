37c02e0c8f27d1a7a93dcb811a6f1ae20c00435e
guojuntao <guojuntao1@gmail.com>
2018-06-11 18:34:08 +0800

    add pprof

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
+
 	"git.finogeeks.club/finochat/go-gin/api"
 	"git.finogeeks.club/finochat/go-gin/config"
 	"git.finogeeks.club/finochat/go-gin/logger"
@@ -8,16 +11,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// TODO: 条件编译，打 tag 的提交不编译 pprof
+	go func() {
+		// /debug/pprof
+		http.ListenAndServe("0.0.0.0:8080", nil)
+	}()
+}
+
 var VERSION string = "unknown"
 
 func main() {
 	cfg := config.GetConfig()
 	gin.SetMode(gin.ReleaseMode)
-	router := gin.New()
-	router.Use(middleware.FinoMonitor())
-	router.Use(gin.Recovery())
+	r := gin.New()
+	r.Use(middleware.FinoMonitor())
+	r.Use(gin.Logger(), gin.Recovery())
 
-	baseGroup := router.Group("/api/v1/", func(c *gin.Context) {})
+	baseGroup := r.Group("/api/v1/", func(c *gin.Context) {})
 	baseGroup.GET("/item/:ID", api.GetHandler)
 	baseGroup.POST("/item/", api.PostHandler)
 	baseGroup.PUT("/item/:ID", api.PutHandler)
@@ -25,5 +36,5 @@ func main() {
 
 	var log = logger.GetLogger()
 	log.Noticef("[gin-demo running... version %s]\n", VERSION)
-	router.Run(":" + cfg.HttpPort)
+	r.Run(":" + cfg.HttpPort)
 }
